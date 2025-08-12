516c4cd200152af1b010e24793cf61c8b3526d61
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2025-08-12 14:16:08 -0300

    Fix deployment issues: disable problematic migration and enable master key

--- db/migrate/99999999999999_migrar_available_days_para_portugues.rb
+++ db/migrate/99999999999999_migrar_available_days_para_portugues.rb
@@ -1,28 +1,31 @@
 class MigrarAvailableDaysParaPortugues < ActiveRecord::Migration[7.1]
-  DIAS = {
-    'monday' => 'segunda-feira',
-    'tuesday' => 'terça-feira',
-    'wednesday' => 'quarta-feira',
-    'thursday' => 'quinta-feira',
-    'friday' => 'sexta-feira',
-    'saturday' => 'sábado',
-    'sunday' => 'domingo'
-  }
+  # Temporarily disabled to fix deployment issues
+  # DIAS = {
+  #   'monday' => 'segunda-feira',
+  #   'tuesday' => 'terça-feira',
+  #   'wednesday' => 'quarta-feira',
+  #   'thursday' => 'quinta-feira',
+  #   'friday' => 'sexta-feira',
+  #   'saturday' => 'sábado',
+  #   'sunday' => 'domingo'
+  # }
 
   def up
-    Professional.find_each do |prof|
-      # Corrigir available_days
-      dias = (prof.available_days || []).map { |d| DIAS[d] || d }
-      prof.update_column(:available_days, dias)
+    # Temporarily disabled to fix deployment issues
+    # Professional.find_each do |prof|
+    #   # Corrigir available_days
+    #   dias = (prof.available_days || []).map { |d| DIAS[d] || d }
+    #   prof.update_column(:available_days, dias)
 
-      # Corrigir available_hours
-      ah = prof.available_hours || {}
-      novo_ah = {}
-      ah.each do |k, v|
-        novo_ah[DIAS[k] || k] = v
-      end
-      prof.update_column(:available_hours, novo_ah)
-    end
+    #   # Corrigir available_hours
+    #   ah = prof.available_hours || {}
+    #   novo_ah = {}
+    #   ah.each do |k, v|
+    #     novo_ah[DIAS[k] || k] = v
+    #   end
+    #   prof.update_column(:available_hours, novo_ah)
+    # end
+    puts "Migration temporarily disabled for deployment"
   end
 
   def down
